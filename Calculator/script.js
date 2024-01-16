143aad9f27ef07345c1c378cd1a2f7015685769f
Dinesh Dhayfule <109637254+dineshdhayfule@users.noreply.github.com>
2024-01-16 17:28:57 +0530

    3RD COMPLETED

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,1 +1,28 @@
-function fun() {alert("hey");  }
+
+function input_value(digit) {
+    document.getElementById("output").innerText += digit;
+}
+
+function clr() 
+{
+      document.getElementById("output").innerText = "0";
+      document.getElementById("history").innerText = "";
+}
+
+
+function result() {
+    var res = document.getElementById("output").innerText;
+          document.getElementById("history").innerText = res;
+    if (res) {
+        document.getElementById("output").innerText = eval(res);
+    }
+}
+
+
+function backspace() {
+         var bs = document.getElementById("output").innerText;
+         document.getElementById("output").innerText = bs.substring(0, bs.length - 1);
+        if (bs=="") {
+            document.getElementById("output").innerText = "0";
+        }
+}
