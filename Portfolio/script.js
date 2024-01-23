18ac9c78d378c0de5401f0ebbb50e34d4b8ee341
Dinesh Dhayfule <109637254+dineshdhayfule@users.noreply.github.com>
2024-01-23 16:55:43 +0530

    Add files via upload

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -1,12 +1,24 @@
-const text = document.querySelector(".role");
-const textLoad = () => {
+const text=document.querySelector(".role");
+const textLoad=()=>{
     setTimeout(() => {
-        text.textContent = "Frontend Developer";
+        text.textContent = "Programer";
     }, 0);
     setTimeout(() => {
-        text.textContent = "Web Developer";
+        text.textContent = "Frontend Developer";
     }, 4000);
+    setTimeout(() => {
+        text.textContent="Web Developer";
+    }, 8000);
 }
 
 textLoad();
-setInterval(textLoad, 8000);
+setInterval(textLoad,12000);
+
+
+function over() {
+    document.getElementById("bt").textContent = "About me ⬇";
+}
+
+function out() {
+    document.getElementById("bt").textContent = "About me ➡";
+}
